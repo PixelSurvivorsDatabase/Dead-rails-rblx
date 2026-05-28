ee82d31943e3b5048a28d35dbf52d84f1ea4d93a
PixelSurvivorsDatabase <lavalordgaming242@gmail.com>
2026-05-27 21:11:46 -0400

    Add shared game config

---
+++ src/ReplicatedStorage/Modules/GameConfig.lua
@@ -0,0 +1,10 @@
+local GameConfig = {
+	StartingMoney = 0,
+	StartingFuel = 100,
+	MaxFuel = 100,
+	FuelDrainPerSecond = 0.25,
+	EventIntervalSeconds = 30,
+	DebugMode = true,
+}
+
+return GameConfig
