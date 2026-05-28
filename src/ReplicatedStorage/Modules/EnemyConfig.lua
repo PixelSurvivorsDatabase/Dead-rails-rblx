802de2c12e97ab1298dc6ffae7e140fd79b097d9
PixelSurvivorsDatabase <lavalordgaming242@gmail.com>
2026-05-27 21:12:13 -0400

    Add enemy config module

---
+++ src/ReplicatedStorage/Modules/EnemyConfig.lua
@@ -0,0 +1,12 @@
+local EnemyConfig = {
+	SpawnIntervalSeconds = 12,
+	MaxActiveEnemies = 10,
+	BasicWalker = {
+		Health = 35,
+		WalkSpeed = 10,
+		Damage = 10,
+		RewardMoney = 5,
+	},
+}
+
+return EnemyConfig
